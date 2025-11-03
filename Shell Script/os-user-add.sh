c9d1c5f89ac02bc479cd4d78c0ceeab75b48ee52
Riva N <rivarahim@gmail.com>
2025-11-03 21:12:14 +0700

    [fix] Fixing user-add script

--- Shell Script/os-user-add.sh
+++ Shell Script/os-user-add.sh
@@ -17,11 +17,23 @@ else
     useradd -m -s (which zsh) $username
     if [ $? -eq 0 ]; then
         echo "User $username has been added successfully."
+
+        echo "Set up password for user $username"
+        passwd $username
+
+        read -p "Add $username to sudo group? (y/n): " option
+        if [[ $option == "y" || $option == "Y" ]]; then
+            usermod -aG sudo $username
+            if [ $? -eq 0 ]; then
+                echo "User $username has been added to the sudo group."
+            else
+                echo "Failed to add user $username to the sudo group."
+            fi
+        else
+            exit 1
+        fi
     else
         echo "Failed to add user $username."
         exit 1
     fi
 fi
-
-echo "Set up password for user $username"
-passwd $username
