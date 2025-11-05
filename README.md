e7be10f70702a7bdfda959dfa5a92fe9fdbd0cbd
Riva N <rivarahim@gmail.com>
2025-11-05 13:33:49 +0700

    [feat] Add password feature to os-user-add

--- README.md
+++ README.md
@@ -3,3 +3,32 @@ This repository is used for my private command collection
 Used for several task, e.g. add user, install shell, packages, and etc.
 
 Feel free to use this repository if you need
+
+# How to run
+## Pre-requisites
+1. Add the `Shell Script` directory into your `PATH`, or simply just go inside it
+2. Don't forget to add execute permission to all of the .sh files.
+
+```
+chmod +x './Shell Script/*.sh'
+```
+
+## Add user
+### Without pre-filled encrypted password
+```
+cd "Shell Script"
+
+./os-user-add.sh -u USERNAME
+```
+
+### With pre-filled encrypted password
+```
+./os-user-add.sh -u USERNAME -p PASSWORD
+```
+or
+```
+./os-user-add.sh -u USERNAME -p $(openssl passwd -6 "PASSWORD")
+```
+> You can get the encrypted password from any other online tools such as [mkpasswd.net](https://www.mkpasswd.net/index.php). \
+> However, it will be much safer to generate the password from your own local device.
+
--- Shell Script/os-user-add.sh
+++ Shell Script/os-user-add.sh
@@ -9,31 +9,68 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-username=$1
+username=""
+encrypted_password=""
+
+while getopts u:p: opt; do
+    case $opt in
+        u)
+            username=$OPTARG
+            ;;
+        p)
+            encrypted_password=$OPTARG
+            ;;
+        *)
+            echo "Usage: $0 -u USERNAME [-p ENCRYPTED_PASSWORD]"
+            exit 1
+            ;;
+    esac
+done
+
+# Check if username is provided
+if [ -z "$username" ]; then
+    echo "Username is required. Use -u option to specify the username."
+    exit 1
+fi
 
 if cat /etc/passwd | grep -q $username; then
     echo "User $username already exists."
 else
-    useradd -m -s $(which bash) $username
-    if [ $? -eq 0 ]; then
-        echo "User $username has been added successfully."
-
-        echo "Set up password for user $username"
-        passwd $username
-
-        read -p "Add $username to sudo group? (y/n): " option
-        if [[ $option == "y" || $option == "Y" ]]; then
-            usermod -aG sudo $username
-            if [ $? -eq 0 ]; then
-                echo "User $username has been added to the sudo group."
-            else
-                echo "Failed to add user $username to the sudo group."
-            fi
-        else
+    if [ -n "$encrypted_password" ]; then
+        # If encrypted_password is exists
+        useradd -m -s $(which bash) -p "$encrypted_password" $username
+        if [ $? -ne 0 ]; then
+            echo "Failed to add user $username."
             exit 1
         fi
     else
-        echo "Failed to add user $username."
-        exit 1
+        # If encrypted_password is not exists
+        useradd -m -s $(which bash) $username
+        if [ $? -ne 0 ]; then
+            echo "Failed to add user $username."
+            exit 1
+        fi
+
+        passwd $username
+        if [ $? -ne 0 ]; then
+            echo "Failed to set password for user $username."
+            exit 1
+        fi
+    fi
+
+    # Add user to sudo group
+    read -p "Add $username to sudo group? (y/n): " group_opt 
+    if [[ $group_opt == "y" || $group_opt == "Y" ]]; then
+        usermod -aG sudo $username
+        if [ $? -eq 0 ]; then
+            echo "User $username has been successfully added to the sudo group."
+        else
+            echo "Failed to add user $username to the sudo group."
+            exit 1
+        fi
+    else
+        echo "User $username has been created without sudo privileges."
     fi
 fi
+
+exit 0
